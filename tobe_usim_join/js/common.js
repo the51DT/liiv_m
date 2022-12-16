70279898daf152289623b1ea2081705d0ea725b5
miok <aluvy_@naver.com>
2022-12-16 14:32:18 +0900

    home bar 제거 함수 추가

--- tobe_usim_join/js/common.js
+++ tobe_usim_join/js/common.js
@@ -3,15 +3,14 @@ window.addEventListener("load", ()=>{
     const index = document.querySelector(".section__type");
     index.classList.add("active");
 
-    const statusBar = document.querySelector(".mobile-hidden");
-    isMobile ? statusBar.style.display = "none" : null;
-    isMobile ? document.querySelector("body").classList.add("mobile") : null;
-
     getHeadPad();
     window.addEventListener("resize", getHeadPad);
-
+    if(isMobile){
+        document.querySelector("body").classList.add("mobile");
+        getFootPad();
+        window.addEventListener("resize", getFootPad);
+    }
     
-
     const linkItem = Array.from(document.querySelectorAll(".link__item"));
     linkItem.forEach((item)=>{
         item.addEventListener("click", (e)=>{ e.preventDefault(); pageChange(item.dataset.link) })
@@ -32,6 +31,12 @@ const getHeadPad = function(){
     const head = document.querySelector("header");
     const contentWrap = document.querySelectorAll(".container__wrap");
     contentWrap.forEach((item)=>{ item.style.marginTop = `${head.offsetHeight}px`; });
+}
+
+// footer height check
+const getFootPad = function(){
+    const foot = document.querySelector("footer");
+    foot.style.bottom = `-${foot.offsetHeight * 0.2}px`;
 }
 
 const pageChange = function(link){
